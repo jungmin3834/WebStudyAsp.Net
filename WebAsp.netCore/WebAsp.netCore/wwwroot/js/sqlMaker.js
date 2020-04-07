ab6aee907818ffd58611512eeae47f8b61610cdd
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-04-07 22:27:49 +0900

    index focus change event (oninput)

--- WebAsp.netCore/WebAsp.netCore/wwwroot/js/sqlMaker.js
+++ WebAsp.netCore/WebAsp.netCore/wwwroot/js/sqlMaker.js
@@ -1,11 +1,12 @@
 ﻿
 const tableScript = () => {
 
-  var tableMaker = document.getElementById('tableMaker').children.item(0);
+    let tableMaker = document.getElementById('tableMaker').children.item(0);
+    let tableName = document.getElementById('tableName').value;
 
     var rowList = [[],[],[],[],[],[],[],[],[],[]];
     for (var i = 1; i < tableMaker.children.length; i++) {
-        var row = tableMaker.children.item(i);
+        let row = tableMaker.children.item(i);
         for (var j = 0; j < row.children.length; j++) {
             if (row.children.item(j).children.item(0).getAttribute("type") == "checkbox"){
                 rowList[j].push(row.children.item(j).children.item(0).checked);
@@ -15,6 +16,14 @@ const tableScript = () => {
             }
         }
     }
-    alert(rowList[0][0] + " " + rowList[6][0]);
-    
+
+    var result = "CREATE TABLE " + tableName + "(" + rowList[0] + " " + rowList[1] + ");";
+    alert(result);
+}
+
+const columnTypeSelect = () => {
+
+    var idx = document.getElementById('dateType').value.indexOf('(', 0) + 1;
+    const input = document.getElementById('dateType').setSelectionRange(idx, idx);
+    input.focus();
 }
