a81337a9c208f279a11214af9aceab107065042d
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-04-07 17:36:59 +0900

    sqlMaker Javascript 생성 및 사용자가 채운 input table 내용 전부 들고 와서 리스트에 저장

---
+++ WebAsp.netCore/WebAsp.netCore/wwwroot/js/sqlMaker.js
@@ -0,0 +1,20 @@
+﻿
+const tableScript = () => {
+
+  var tableMaker = document.getElementById('tableMaker').children.item(0);
+
+    var rowList = [[],[],[],[],[],[],[],[],[],[]];
+    for (var i = 1; i < tableMaker.children.length; i++) {
+        var row = tableMaker.children.item(i);
+        for (var j = 0; j < row.children.length; j++) {
+            if (row.children.item(j).children.item(0).getAttribute("type") == "checkbox"){
+                rowList[j].push(row.children.item(j).children.item(0).checked);
+            }
+            else if (row.children.item(j).children.item(0).getAttribute("type") == "text") {
+                rowList[j].push(row.children.item(j).children.item(0).value);
+            }
+        }
+    }
+    alert(rowList[0][0] + " " + rowList[6][0]);
+    
+}
